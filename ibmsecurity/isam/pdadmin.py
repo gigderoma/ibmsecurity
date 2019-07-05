65d001c7cab500ed9c81a3d3c428f556a95f6a4d
gianluca <g.gargaro@gmail.com>
2019-07-05 14:05:17 +0200

    Update pdadmin.py

--- ibmsecurity/isam/pdadmin.py
+++ ibmsecurity/isam/pdadmin.py
@@ -1,1 +1,14 @@
+import logging
+from ibmsecurity.utilities import tools
 
+logger = logging.getLogger(__name__)
+
+
+def run(isamAppliance, application_interface, statistics_duration, check_mode=False, force=False):
+    """
+    Retrieving the Application Interface Statistics
+    """
+    return isamAppliance.invoke_get("Run pdadmin command",
+                                    "/analysis/interface_statistics.json{0}".format(
+                                        tools.create_query_string(prefix=application_interface,
+timespan=statistics_duration)))
